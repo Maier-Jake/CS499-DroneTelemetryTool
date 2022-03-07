72434a385534f5397dac4279c698fc43df4fb600
Jake Maier <30730691+Maier-Jake@users.noreply.github.com>
2022-03-06 20:48:24 -0600

    Squashed commit of the following:
    commit 6e7f0f167697bc82788747557cb6c47e6dbc638a
    Author: Jake Maier <30730691+Maier-Jake@users.noreply.github.com>
    Date:   Sun Mar 6 20:45:57 2022 -0600
    
        Character and Text Gauge
    
    commit bdfbabb212bdfbe71d3c645d93d3cd73a86efd6c
    Author: Jake Maier <30730691+Maier-Jake@users.noreply.github.com>
    Date:   Sun Mar 6 20:35:27 2022 -0600
    
        On/Off Gauge
    
        Customization: Title, on color

---
+++ src/main/java/dronetelemetrytool/fxml/OnOffGaugeCreator.java
@@ -0,0 +1,70 @@
+package dronetelemetrytool.fxml;
+
+import dronetelemetrytool.MainApplication;
+import dronetelemetrytool.gauges.OnOffGauge;
+import eu.hansolo.tilesfx.colors.Bright;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+
+public class OnOffGaugeCreator implements Initializable {
+    @FXML
+    private Label HEADER;
+    @FXML
+    private TextField FIELD_Title;
+    @FXML
+    private ColorPicker COLOR_on;
+    @FXML
+    private Button BUTTON_Close;
+
+    @FXML
+    protected void onCancelClick() {
+
+        System.out.println("Cancelled creating this gauge.");
+
+        //Finished, close this Gauge Creation window.
+        Stage stage = (Stage) BUTTON_Close.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle rb) {
+
+        COLOR_on.setValue(Bright.GREEN);
+    }
+
+
+    @FXML
+    protected void onCompletedClick() throws IOException {
+        //Nothing to verify, the user can't mess this one up.
+        createGauge();
+
+        //Finished, close this Gauge Creation window.
+        Stage stage = (Stage) BUTTON_Close.getScene().getWindow();
+        stage.close();
+    }
+
+    private void createGauge()
+    {
+        String title = FIELD_Title.textProperty().getValueSafe();
+        OnOffGauge newGauge = new OnOffGauge();
+        newGauge.setTitle(title);
+
+
+        Color on = COLOR_on.getValue();
+        newGauge.tile.setActiveColor(on);
+
+
+        MainApplication.gauges.add(newGauge);
+        newGauge.display();
+    }
+}
