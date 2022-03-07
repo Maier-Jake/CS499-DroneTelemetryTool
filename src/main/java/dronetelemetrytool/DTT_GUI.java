6e7f0f167697bc82788747557cb6c47e6dbc638a
Jake Maier <30730691+Maier-Jake@users.noreply.github.com>
2022-03-06 20:45:57 -0600

    Character and Text Gauge

--- src/main/java/dronetelemetrytool/DTT_GUI.java
+++ src/main/java/dronetelemetrytool/DTT_GUI.java
@@ -30,4 +30,26 @@ public class DTT_GUI {
         newStage.setMaxHeight(600); newStage.setMinHeight(470);
         newStage.setMaxWidth(1000); newStage.setMinWidth(800);
     }
+    public static void characterGaugeCreator() throws IOException {
+        Stage newStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("fxml/characterGaugeCreator_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(MainApplication.class.getResource("fxml/DTT_Style.css").toExternalForm());
+        newStage.setTitle("DTT - Gauge Creation Tool");
+        newStage.setScene(scene);
+        newStage.show();
+        newStage.setMaxHeight(600); newStage.setMinHeight(470);
+        newStage.setMaxWidth(800); newStage.setMinWidth(600);
+    }
+    public static void textGaugeCreator() throws IOException {
+        Stage newStage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("fxml/textGaugeCreator_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(MainApplication.class.getResource("fxml/DTT_Style.css").toExternalForm());
+        newStage.setTitle("DTT - Gauge Creation Tool");
+        newStage.setScene(scene);
+        newStage.show();
+        newStage.setMaxHeight(600); newStage.setMinHeight(470);
+        newStage.setMaxWidth(800); newStage.setMinWidth(600);
+    }
 }
--- src/main/java/dronetelemetrytool/MainApplication.java
+++ src/main/java/dronetelemetrytool/MainApplication.java
@@ -74,7 +74,7 @@ public class MainApplication extends Application {
         //DTT_Tools.displayVideo(video);
         timer.start();
 
-        DTT_GUI.onOffGaugeCreator();
+        DTT_GUI.characterGaugeCreator();
 
     }
 
