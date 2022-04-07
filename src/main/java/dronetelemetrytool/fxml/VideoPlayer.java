450e1a5ed46b3c1ca20f8b4ce3831c8befa55842
EJ McKinley <hcseej@gmail.com>
2022-04-07 10:44:37 -0500

    videoPlayerButtons have functionality

--- src/main/java/dronetelemetrytool/fxml/VideoPlayer.java
+++ src/main/java/dronetelemetrytool/fxml/VideoPlayer.java
@@ -13,8 +13,10 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,6 +30,16 @@ public class VideoPlayer implements Initializable {
     private MediaView mediaView;
 //    @FXML
 //    private HBox mediaBox;
+    @FXML
+    private Button reverseButton;
+    @FXML
+    private Button pauseButton;
+    @FXML
+    private Button forwardButton;
+    @FXML
+    private Button fiveSpeedButton;
+    @FXML
+    private Button eigthSpeedButton;
 
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -35,6 +47,29 @@ public class VideoPlayer implements Initializable {
         MediaPlayer mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setAutoPlay(false);
         mediaView.setMediaPlayer(mediaPlayer);
+    }
+
+    public void reverseClick() {
+        mediaView.getMediaPlayer().seek(Duration.ZERO);
+    }
+
+    public void pauseClick() {
+        mediaView.getMediaPlayer().pause();
+    }
+
+    public void forwardClick() {
+        mediaView.getMediaPlayer().setRate(1.0);
+        mediaView.getMediaPlayer().play();
+    }
+
+    public void fiveSpeedClick() {
+        mediaView.getMediaPlayer().setRate(5.0);
+        mediaView.getMediaPlayer().play();
+    }
+
+    public void eightSpeedClick() {
+        mediaView.getMediaPlayer().setRate(8.0);
+        mediaView.getMediaPlayer().play();
     }
 
 }
