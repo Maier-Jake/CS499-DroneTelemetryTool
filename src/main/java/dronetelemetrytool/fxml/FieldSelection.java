2737048cba3f2200f9ac709e7dffc05464c1693a
Swwetsy <ncv0001@uah.edu>
2022-04-11 14:40:33 -0500

    Prototype for Field Selection

--- src/main/java/dronetelemetrytool/fxml/FieldSelection.java
+++ src/main/java/dronetelemetrytool/fxml/FieldSelection.java
@@ -1,14 +1,32 @@
 package dronetelemetrytool.fxml;
 
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class FieldSelection implements Initializable {
+
+    @FXML
+    public ListView leftView;
+
+    @FXML
+    public ListView rightView;
+
     @FXML
     protected void onCreateClick() {
 
@@ -31,7 +49,25 @@ public class FieldSelection implements Initializable {
 
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
-        //ObservableList<String> fields = FXCocllections.observableArrayList("Gauge 1","Gauge 2", "Gauge 3");
-        //ListView<String> listView = new ListView<String>(fields);
+        ObservableList<String> leftFields = FXCollections.observableArrayList();
+
+        ArrayList<String> leftSet = new ArrayList<>();
+        leftSet.add("String 1");
+        leftSet.add("String 2");
+        leftSet.add("String 3");
+        leftSet.add("String 4");
+        leftFields.setAll(leftSet);
+
+        leftView.setItems(leftFields);
+        //leftView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+
+        ObservableList<String> rightFields = FXCollections.observableArrayList();
+        ArrayList<String> rightSet = new ArrayList<>();
+        rightFields.setAll(rightSet);
+
+        rightView.setItems(rightFields);
+
+        //rightView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 }
