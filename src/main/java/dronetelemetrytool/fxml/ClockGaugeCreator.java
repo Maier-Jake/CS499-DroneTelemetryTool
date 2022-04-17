668198b9d26f145bc68aa1cfe3a59849a48bcd04
Jake Maier <30730691+Maier-Jake@users.noreply.github.com>
2022-04-17 18:21:35 -0500

    Fixed Non-Stat gauges from crashing
    They were still looking to update the stats, (that no longer exist)

--- src/main/java/dronetelemetrytool/fxml/ClockGaugeCreator.java
+++ src/main/java/dronetelemetrytool/fxml/ClockGaugeCreator.java
@@ -17,25 +17,10 @@ import java.util.ResourceBundle;
 
 public class ClockGaugeCreator implements Initializable {
     @FXML
-    private Label HEADER;
-    @FXML
     private TextField FIELD_Title;
     @FXML
     private Button BUTTON_Close;
-    @FXML
-    private TextField STAT_max;
-    @FXML
-    private TextField STAT_min;
-    @FXML
-    private TextField STAT_avg;
-    @FXML
-    private TextField STAT_stddev;
-    @FXML
-    private ComboBox<String> unitTypeComboBox;
-    @FXML
-    private ComboBox<String> currentUnitComboBox;
-    @FXML
-    private ComboBox<String> desiredUnitComboBox;
+
 
     @FXML
     protected void onCancelClick() {
@@ -48,20 +33,7 @@ public class ClockGaugeCreator implements Initializable {
 
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
-        STAT_min.setText("10");
-        STAT_max.setText("20");
-        STAT_avg.setText("12");
-        STAT_stddev.setText("2");
 
-        //so focus will start on first editable textfield
-        STAT_min.setFocusTraversable(false);
-        STAT_max.setFocusTraversable(false);
-        STAT_avg.setFocusTraversable(false);
-        STAT_stddev.setFocusTraversable(false);
-
-        unitTypeComboBox.getItems().setAll("speed", "length");
-        currentUnitComboBox.getItems().setAll("m/s", "ft/s", "mph", "m", "ft", "mi");
-        desiredUnitComboBox.getItems().setAll("m/s", "ft/s", "mph", "m", "ft", "mi");
     }
 
 
